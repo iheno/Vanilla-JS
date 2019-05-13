1698d309efacf5a569ae2906b6edcbe72fd051b2
Antonio-Jung <antonio_jung@wamazing.jp>
2019-05-13 19:46:19 +0900

    data types

--- index.js
+++ index.js
@@ -3,7 +3,15 @@
 // then : Initialize -> 초기화
 // last : Use
 
-const a = 221;
-let b = a - 5;
-a = 4; 
-console.log(b,a)
+//const 는 상수다 바꿀수 없다. let 만이 변수 값을 바꿀수 있다.
+
+// 변수를 선언 할때는 기본적으로 const를 쓰자 내가 팔요할 때만 let사용
+
+/* String 
+const what = "antonio";
+console.log(what);
+*/
+
+// Boolern true: 1 false : 0
+const wat = true;
+console.log(wat);
